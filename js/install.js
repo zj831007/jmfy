4e6f1109b90cd3e206c99aef7df869898f746d6a
Jian Zhang <zhangjian@jingmufengyu.com>
2015-11-12 16:19:19 +0800

    test

--- js/install.js
+++ js/install.js
@@ -1,4 +1,6 @@
 function openInstallURL() {
-  var url = 'itms-services://?action=download-manifest&url=https://zj831007.github.io/jmfy/res/install.plist';
+  // var url = 'itms-services://?action=download-manifest&url=https://zj831007.github.io/jmfy/res/install.plist';
+var url = 'itms-services://?action=download-manifest&url=https://qzs21.github.io/iComic/res/install.plist';
+
   window.self.location = url;
 }
